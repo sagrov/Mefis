b53aea1f26a0ea887f2d4c9a24a609d11953b20b
sagrov <howlxfamily@gmail.com>
2023-05-24 22:39:08 +0300

    v0.4

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -2,20 +2,19 @@ $(function() {
   // Owl Carousel
   var owl = $(".owl-carousel");
   owl.owlCarousel({
+
     items: checking(),
     margin: 10,
     loop: true,
     nav: true
   });
 });
-
+window.dispatchEvent(new Event('resize'));
 function checking() {
-  if (self.innerWidth <= 400) {
+  if (self.innerWidth <= 450) {
     return items = 1;
   }
   else {
-    return items = 3
+    return items = 3;
   }
 }
-
-
