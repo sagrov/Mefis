bca34c6bfea6bd56a5926d0c6bd7c3be3f47b945
sagrov <howlxfamily@gmail.com>
2023-05-29 19:37:40 +0300

    product page -> three images

--- main/views.py
+++ main/views.py
@@ -12,36 +12,36 @@ def index(request):
     return render(request, 'main/index.html', {'products': products, 'products_six': products_six, 'categories': categories })
 
 
-def catalog(request):
-    # if request.method == "GET":
-    #     request_category = request.GET.get("category")
-    #     request_subcategory = request.GET.get("subcategory")
-    #     if request.GET.get("category"):
-    #         if request_category == "Стільці":
-    #             products = Product.objects.filter(category=2)
-    #         elif request_category == "Столи":
-    #             products = Product.objects.filter(category=3)
-    #         elif request_category == "Лампи":
-    #             products = Product.objects.filter(category=4)
-    #         elif request_category == "Ліжка":
-    #             products = Product.objects.filter(category=5)
-    #         elif request_category == "Дивани":
-    #             products = Product.objects.filter(category=6)
-    #         else:
-    #             products = Product.objects.all()
-    #
-    #     if request.GET.get("subcategory"):
-    #         if request_subcategory == "Домашні":
-    #             products = Product.objects.filter(subcategory=2)
-    #         if request_subcategory == "Офісні":
-    #             products = Product.objects.filter(subcategory=3)
-    #         if request_subcategory == "Всі товари":
-    #             products = Product.objects.all()
-    categories = Categories.objects.all()
-    products = Product.objects.filter(category=request.GET.get("category"))
-    print(request.GET.get("category"))
-    subcategories = Subcategories.objects.all()
-    return render(request, 'main/catalog.html', {'products': products, 'categories': categories, 'subcategories': subcategories})
+# def catalog(request):
+#     # if request.method == "GET":
+#     #     request_category = request.GET.get("category")
+#     #     request_subcategory = request.GET.get("subcategory")
+#     #     if request.GET.get("category"):
+#     #         if request_category == "Стільці":
+#     #             products = Product.objects.filter(category=2)
+#     #         elif request_category == "Столи":
+#     #             products = Product.objects.filter(category=3)
+#     #         elif request_category == "Лампи":
+#     #             products = Product.objects.filter(category=4)
+#     #         elif request_category == "Ліжка":
+#     #             products = Product.objects.filter(category=5)
+#     #         elif request_category == "Дивани":
+#     #             products = Product.objects.filter(category=6)
+#     #         else:
+#     #             products = Product.objects.all()
+#     #
+#     #     if request.GET.get("subcategory"):
+#     #         if request_subcategory == "Домашні":
+#     #             products = Product.objects.filter(subcategory=2)
+#     #         if request_subcategory == "Офісні":
+#     #             products = Product.objects.filter(subcategory=3)
+#     #         if request_subcategory == "Всі товари":
+#     #             products = Product.objects.all()
+#     categories = Categories.objects.all()
+#     products = Product.objects.filter(category=request.GET.get("category"))
+#     print(request.GET.get("category"))
+#     subcategories = Subcategories.objects.all()
+#     return render(request, 'main/catalog.html', {'products': products, 'categories': categories, 'subcategories': subcategories})
 
 
 def category(request, category_id):
@@ -55,8 +55,10 @@ def category(request, category_id):
 def product(request, product_real_id):
     categories = Categories.objects.all()
     main_product = Product.objects.get(id=product_real_id)
-    main_product_images = ProductImage.objects.filter(main_product=main_product)
+    main_product_images = ProductImage.objects.filter(product__id=product_real_id)
+    print(main_product_images)
     products = Product.objects.all()
+    print(products)
     return render(request, 'main/product.html', {'head_product': main_product, 'categories': categories, 'products': products, 'head_product_images': main_product_images})
 
 
@@ -75,8 +77,9 @@ def catalog(request):
         if request_category != '1':
             if request.GET.get("category"):
                 products = Product.objects.filter(category=request_category)
-        if request.GET.get("subcategory"):
-            products = Product.objects.filter(subcategory=request_subcategory)
+        if request_subcategory != '1':
+            if request.GET.get("subcategory"):
+                products = Product.objects.filter(subcategory=request_subcategory)
     categories = Categories.objects.all()
     subcategories = Subcategories.objects.all()
     return render(request, 'main/catalog.html',
